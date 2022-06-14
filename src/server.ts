55af7271666e6096ccf1ac387710bdb3ecb0d23f
Edidiong Udoh(TechnicalDee) <gisthive@gmail.com>
2022-06-14 22:58:53 +0100

    to github

---
+++ src/server.ts
@@ -0,0 +1,61 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {filterImageFromURL, deleteLocalFiles} from './util/util';
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+  
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+  app.get( "/filteredimage", async ( req, res ) => {
+    try{
+      let {image_url} = req.query;
+      if(!image_url){
+        return res.status(422).send("server cannott process your request");
+      }
+      console.log(image_url);
+      const path = await filterImageFromURL(image_url);
+      res.status(200).sendFile(path);
+      res.on('finish', () => deleteLocalFiles([path]));
+    } catch (err) {
+      console.log(err)
+      return res.status(500).send({error: "Something terribly wrong! Image size is probably too large."});
+    }
+  } );
+  // IT SHOULD
+  //    1
+  //    1. validate the image_url query
+  //    2. call filterImageFromURL(image_url) to filter the image
+  //    3. send the resulting file in the response
+  //    4. deletes any files on the server on finish of the response
+  // QUERY PARAMATERS
+  //    image_url: URL of a publicly accessible image
+  // RETURNS
+  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+  /**************************************************************************** */
+
+  //! END @TODO1
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req, res ) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
